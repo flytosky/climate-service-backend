56aa0b0f4533f9ee3521a622b3a99dfd2c016ba8
Zelin-Zeng <wernerzeng@gmail.com>
2015-02-26 19:01:42 -0800

    add ServiceExecutionLogController
    add ServiceExecutionLogController

---
+++ app/controllers/ServiceExecutionLogController.java
@@ -0,0 +1,222 @@
+package controllers;
+
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.persistence.PersistenceException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.Gson;
+
+import play.mvc.*;
+import models.ClimateService;
+import models.ClimateServiceRepository;
+//import models.DatasetLog;
+import models.DatasetLogRepository;
+import models.ServiceConfiguration;
+import models.ServiceConfigurationRepository;
+import models.ServiceExecutionLog;
+import models.ServiceExecutionLogRepository;
+import models.User;
+import models.UserRepository;
+
+
+/**
+ * The main set of web services.
+ */
+@Named
+@Singleton
+public class ServiceExecutionLogController extends Controller {
+	private final ServiceExecutionLogRepository serviceExecutionLogRepository;
+    private final UserRepository userRepository;
+    private final ClimateServiceRepository climateServiceRepository;
+    //private final DatasetLogRepository datasetLogRepository;
+    private final ServiceConfigurationRepository serviceConfigurationRepository;
+	
+	// We are using constructor injection to receive a repository to support our desire for immutability.
+    @Inject
+    public ServiceExecutionLogController(
+			ServiceExecutionLogRepository serviceExecutionLogRepository,
+			UserRepository userRepository,
+			ClimateServiceRepository climateServiceRepository,
+			DatasetLogRepository datasetLogRepository,
+			ServiceConfigurationRepository serviceConfigurationRepository) {
+		this.serviceExecutionLogRepository = serviceExecutionLogRepository;
+		this.userRepository = userRepository;
+		this.climateServiceRepository = climateServiceRepository;
+		//this.datasetLogRepository = datasetLogRepository;
+		this.serviceConfigurationRepository = serviceConfigurationRepository;
+	}
+    
+    public Result addServiceExecutionLog() {
+    	JsonNode json = request().body().asJson();
+    	if (json == null) {
+    		System.out.println("ServiceExecutionLog not saved, expecting Json data");
+			return badRequest("ServiceExecutionLog not saved, expecting Json data");
+    	}
+
+    	//Parse JSON file
+    	long serviceId = json.findPath("serviceId").asLong();
+    	long userId = json.findPath("userId").asLong();
+    	long serviceConfigurationId = json.findPath("serviceConfigurationId").asLong();
+    	//long datasetLogId = json.findPath("datasetLogId").asLong();
+    	String purpose = json.findPath("purpose").asText();
+    	String plotUrl = json.findPath("url").asText();
+    	String dataUrl = json.findPath("dataUrl").asText();
+    	String executionStartTimeString = json.findPath("executionStartTime").asText();
+    	String executionEndTimeString = json.findPath("executionEndTime").asText();
+    	Date executionStartTime = new Date();
+    	Date executionEndTime = new Date();
+    	SimpleDateFormat simpleDateFormat = new SimpleDateFormat(util.Common.DATE_PATTERN);
+    	
+    	try {
+    		executionStartTime = simpleDateFormat.parse(executionStartTimeString);
+    	} catch (ParseException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    		System.out.println("Wrong Date Format :" + executionStartTimeString);
+    		return badRequest("Wrong Date Format :" + executionStartTimeString);
+    	}
+    	try {
+    		executionEndTime = simpleDateFormat.parse(executionEndTimeString);
+    	} catch (ParseException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    		System.out.println("Wrong Date Format :" + executionEndTimeString);
+    		return badRequest("Wrong Date Format :" + executionEndTimeString);
+    	}
+		try {
+			User user = userRepository.findOne(userId);
+			ClimateService climateService = climateServiceRepository.findOne(serviceId);
+			ServiceConfiguration serviceConfiguration = serviceConfigurationRepository.findOne(serviceConfigurationId);
+			//DatasetLog datasetLog = datasetLogRepository.findOne(datasetLogId);
+			//ServiceExecutionLog ServiceExecutionLog = new ServiceExecutionLog(climateService, user, serviceConfiguration, datasetLog, purpose, executionStartTime, executionEndTime);
+			ServiceExecutionLog ServiceExecutionLog = new ServiceExecutionLog(climateService, user, serviceConfiguration, purpose, executionStartTime, executionEndTime, dataUrl, plotUrl);
+			ServiceExecutionLog savedServiceExecutionLog = serviceExecutionLogRepository.save(ServiceExecutionLog);
+			
+			System.out.println("ServiceExecutionLog saved: " + savedServiceExecutionLog.getId());
+			return created(new Gson().toJson(savedServiceExecutionLog.getId()));
+		} catch (PersistenceException pe) {
+			pe.printStackTrace();
+			System.out.println(pe.getClass().toString());
+			System.out.println("ServiceExecutionLog not saved");
+			return badRequest("ServiceExecutionLog not saved");
+		}			
+    }
+    
+    
+
+	public Result deleteServiceExecutionLog(Long id) {
+    	ServiceExecutionLog serviceExecutionLog = serviceExecutionLogRepository.findOne(id);
+    	if (serviceExecutionLog == null) {
+    		System.out.println("ServiceExecutionLog not found with id: " + id);
+			return notFound("ServiceExecutionLog not found with id: " + id);
+    	}
+    	
+    	serviceExecutionLogRepository.delete(serviceExecutionLog);
+    	System.out.println("ServiceExecutionLog is deleted: " + id);
+		return ok("ServiceExecutionLog is deleted: " + id);
+    }
+    
+    public Result updateServiceExecutionLog(long id) {
+    	JsonNode json = request().body().asJson();
+    	if (json == null) {
+    		System.out.println("ServiceExecutionLog not saved, expecting Json data");
+			return badRequest("ServiceExecutionLog not saved, expecting Json data");
+    	}
+
+    	//Parse JSON file
+    	long serviceId = json.findPath("serviceId").asLong();
+    	long userId = json.findPath("userId").asLong();
+    	long serviceConfigurationId = json.findPath("serviceConfigurationId").asLong();
+    	//long datasetLogId = json.findPath("datasetLogId").asLong();
+    	String purpose = json.findPath("purpose").asText();
+    	String executionStartTimeString = json.findPath("executionStartTime").asText();
+    	String executionEndTimeString = json.findPath("executionEndTime").asText();
+    	String plotUrl = json.findPath("url").asText();
+    	String dataUrl = json.findPath("dataUrl").asText();
+    	Date executionStartTime = new Date();
+    	Date executionEndTime = new Date();
+    	SimpleDateFormat simpleDateFormat = new SimpleDateFormat(util.Common.DATE_PATTERN);
+    	
+    	try {
+    		executionStartTime = simpleDateFormat.parse(executionStartTimeString);
+    	} catch (ParseException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    		System.out.println("Wrong Date Format :" + executionStartTimeString);
+    		return badRequest("Wrong Date Format :" + executionStartTimeString);
+    	}
+    	try {
+    		executionEndTime = simpleDateFormat.parse(executionEndTimeString);
+    	} catch (ParseException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    		System.out.println("Wrong Date Format :" + executionEndTimeString);
+    		return badRequest("Wrong Date Format :" + executionEndTimeString);
+    	}
+    	
+		try {
+			User user = userRepository.findOne(userId);
+			ClimateService climateService = climateServiceRepository.findOne(serviceId);
+			ServiceConfiguration serviceConfiguration = serviceConfigurationRepository.findOne(serviceConfigurationId);
+			//DatasetLog datasetLog = datasetLogRepository.findOne(datasetLogId);
+			
+			ServiceExecutionLog serviceExecutionLog = serviceExecutionLogRepository.findOne(id);
+			
+			serviceExecutionLog.setClimateService(climateService);
+			serviceExecutionLog.setDataUrl(dataUrl);
+			serviceExecutionLog.setPlotUrl(plotUrl);
+			//serviceExecutionLog.setDatasetLog(datasetLog);
+			serviceExecutionLog.setExecutionEndTime(executionEndTime);
+			serviceExecutionLog.setExecutionStartTime(executionStartTime);
+			serviceExecutionLog.setPurpose(purpose);
+			serviceExecutionLog.setUser(user);
+			serviceExecutionLog.setServiceConfiguration(serviceConfiguration);
+			
+			ServiceExecutionLog savedServiceExecutionLog = serviceExecutionLogRepository.save(serviceExecutionLog);
+			
+			System.out.println("ServiceExecutionLog updated: " + savedServiceExecutionLog.getId());
+			return created("ServiceExecutionLog updated: " + savedServiceExecutionLog.getId());
+		} catch (PersistenceException pe) {
+			pe.printStackTrace();
+			System.out.println("ServiceExecutionLog not updated: " + id);
+			return badRequest("ServiceExecutionLog not updated: " + id);
+		}			
+    }
+    
+    public Result getServiceExecutionLog(Long id, String format) {
+    	if (id < 0) {
+    		System.out.println("id is negative!");
+			return badRequest("id is negative!");
+    	}
+    	
+    	ServiceExecutionLog ServiceExecutionLog = serviceExecutionLogRepository.findOne(id);
+    	if (ServiceExecutionLog == null) {
+    		System.out.println("ServiceExecutionLog not found with id: " + id);
+			return notFound("ServiceExecutionLog not found with id: " + id);
+    	}
+    	
+    	String result = new String();
+    	if (format.equals("json")) {
+    		result = new Gson().toJson(ServiceExecutionLog);
+    	}
+    	
+    	return ok(result);
+    }
+    
+    public Result getAllServiceExecutionLogs(String format) {
+    	
+    	String result = new String();
+    	
+    	if (format.equals("json")) {
+    		result = new Gson().toJson(serviceExecutionLogRepository.findAll());
+    	}
+    			
+    	return ok(result);
+    }
+}
--- app/models/ServiceExecutionLog.java
+++ app/models/ServiceExecutionLog.java
@@ -11,6 +11,8 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class ServiceExecutionLog {
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
@@ -23,79 +25,148 @@ public class ServiceExecutionLog {
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "serviceConfigurationId", referencedColumnName = "id")
 	private ServiceConfiguration serviceConfiguration;
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "datasetLogId", referencedColumnName = "id")
-	private DatasetLog datasetLog;
+//	@ManyToOne(optional = false)
+//	@JoinColumn(name = "datasetLogId", referencedColumnName = "id")
+//	private DatasetLog datasetLog;
 	private String purpose;
 	private Date executionStartTime;
 	private Date executionEndTime;
+	private String plotUrl;
+	private String dataUrl;
+	
 	public ServiceExecutionLog(ClimateService climateService,
-			User user, ServiceConfiguration serviceConfiguration, DatasetLog datasetLog,
-			String purpose, Date executionStartTime, Date executionEndTime) {
+			User user, ServiceConfiguration serviceConfiguration, //DatasetLog datasetLog,
+			String purpose, Date executionStartTime, Date executionEndTime, String dataUrl, String plotUrl) {
 		this.climateService = climateService;
 		this.user = user;
 		this.serviceConfiguration = serviceConfiguration;
-		this.datasetLog = datasetLog;
+		//this.datasetLog = datasetLog;
 		this.purpose = purpose;
 		this.executionStartTime = executionStartTime;
 		this.executionEndTime = executionEndTime;
+		this.plotUrl = plotUrl;
+		this.dataUrl = dataUrl;
 	}
+	
 	public long getId() {
 		return id;
 	}
+
+
 	public ClimateService getClimateService() {
 		return climateService;
 	}
+
+
 	public User getUser() {
 		return user;
 	}
+
+
 	public ServiceConfiguration getServiceConfiguration() {
 		return serviceConfiguration;
 	}
-	public DatasetLog getDatasetLog() {
-		return datasetLog;
-	}
+
+
+//	public DatasetLog getDatasetLog() {
+//		return datasetLog;
+//	}
+
+
 	public String getPurpose() {
 		return purpose;
 	}
+
+
 	public Date getExecutionStartTime() {
 		return executionStartTime;
 	}
+
+
 	public Date getExecutionEndTime() {
 		return executionEndTime;
 	}
+
+
+	public String getPlotUrl() {
+		return plotUrl;
+	}
+
+
+	public String getDataUrl() {
+		return dataUrl;
+	}
+
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
+
 	public void setClimateService(ClimateService climateService) {
 		this.climateService = climateService;
 	}
+
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
+
 	public void setServiceConfiguration(ServiceConfiguration serviceConfiguration) {
 		this.serviceConfiguration = serviceConfiguration;
 	}
-	public void setDatasetLog(DatasetLog datasetLog) {
-		this.datasetLog = datasetLog;
-	}
+
+
+//	public void setDatasetLog(DatasetLog datasetLog) {
+//		this.datasetLog = datasetLog;
+//	}
+
+
 	public void setPurpose(String purpose) {
 		this.purpose = purpose;
 	}
+
+
 	public void setExecutionStartTime(Date executionStartTime) {
 		this.executionStartTime = executionStartTime;
 	}
+
+
 	public void setExecutionEndTime(Date executionEndTime) {
 		this.executionEndTime = executionEndTime;
 	}
+
+
+	public void setPlotUrl(String plotUrl) {
+		this.plotUrl = plotUrl;
+	}
+
+
+	public void setDataUrl(String dataUrl) {
+		this.dataUrl = dataUrl;
+	}
+
 	@Override
 	public String toString() {
 		return "ServiceExecutionLog [id=" + id + ", climateService="
-				+ climateService + ", user=" + user
-				+ ", serviceConfiguration=" + serviceConfiguration
-				+ ", datasetLog=" + datasetLog + ", purpose=" + purpose
+				+ climateService + ", user=" + user + ", serviceConfiguration="
+				+ serviceConfiguration + ", purpose=" + purpose
 				+ ", executionStartTime=" + executionStartTime
-				+ ", executionEndTime=" + executionEndTime + "]";
+				+ ", executionEndTime=" + executionEndTime + ", plotUrl="
+				+ plotUrl + ", dataUrl=" + dataUrl + "]";
 	}
+
+	
+//	@Override
+//	public String toString() {
+//		return "ServiceExecutionLog [id=" + id + ", climateService="
+//				+ climateService + ", user=" + user
+//				+ ", serviceConfiguration=" + serviceConfiguration
+//				+ ", datasetLog=" + datasetLog + ", purpose=" + purpose
+//				+ ", executionStartTime=" + executionStartTime
+//				+ ", executionEndTime=" + executionEndTime + "]";
+//	}
+	
 	
 }
