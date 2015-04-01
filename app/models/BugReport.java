00b8e6b3926dc68b26820acf46effe1d14d6a563
Rao Li <rao.li@sv.cmu.edu>
2015-04-01 15:39:44 -0700

    Added model files for bug report

---
+++ app/models/BugReport.java
@@ -0,0 +1,116 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+
+import java.util.Date;
+
+/**
+ * Created by raoli on 4/1/15.
+ */
+@Entity
+public class BugReport {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private int id;
+
+		private String title;
+		private String email;
+		private String name;
+		private String organization;
+		private String description;
+		private int solved = 0;
+		public Date creationDate;
+		public Date updateDate;
+		public BugReport() {
+
+		}
+
+		public BugReport(String title, String email, String name,
+						 String organization, String description, int solved,
+						 Date creationDate, Date updateDate) {
+			super();
+			this.title = title;
+			this.email = email;
+			this.name = name;
+			this.organization = organization;
+			this.description = description;
+			this.solved = solved;
+			this.creationDate = creationDate;
+			this.updateDate = updateDate;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String toString() {
+			return "BugReport #" + id;
+		}
+
+		public String getTitle() {
+			return this.title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public String getDescription() {
+			return this.description;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getOrganization() {
+			return organization;
+		}
+
+		public void setOrganization(String organization) {
+			this.organization = organization;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public int getSolved() {
+			return solved;
+		}
+
+		public void setSolved(int solved) {
+			this.solved = solved;
+		}
+
+
+	}
